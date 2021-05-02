cda76d03fc6119fad2a66de39e14a8575789dd4d
Stephanie Lee <stephaniemjlee@gmail.com>
2021-05-03 08:56:34 +1000

    updated write function

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,12 @@
 // TODO: Include packages needed for this application
-// Install Inquirer
-
-// TODO: Create an array of questions for user input
-const questions = [];
 
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
+const generateMarkdown = require("./utils/generateMarkdown");
 
-const writeFileAsync = util.promisify(fs.writeToFile);
+
+// TODO: Create an array of questions for user input
 
 const promptUser = () => {
     return inquirer.prompt ([
@@ -67,49 +65,23 @@ const promptUser = () => {
 
 
 // TODO: Create a function to write README file
-function writeToFile(answers) {
-    return `# ${answers.project_title}
-
-    # Good ReadMe Generator
-
-    ## Table of Contents
-    * [Project Description](#project-description)
-    * [Installation Instructions](#installation)
-    * [Usage Guidelines](#usage-guidelines)
-    * [License](#license)
-    * [Contribution Guidelines](#contribution-guidelines)
-    * [Test Instructions](#test-instructions)
-    * [Questions](#questions)
-
-    # Project Description 
-    * ${answers.description}
-
-    # Installation Instructions
-
-    # Usage Guidelines
-    * ${answers.usage}
-
-    # License
-    * This project is licensed under the ${answers.license}
-
-    # Contribution Guidelines
-    * ${answers.contributions}
-
-    # Test Instructions
-    * ${answers.test_command}
-
-    ## Questions
-    If you have any questions about the repom contact me directly at ${answers.email}. 
-    You can find more of my work at [${answers.github_username}](http://github.com/${answers.github_username}).`;
-}
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // TODO: Create a function to initialize app
-const init = () => {
+
+function init() {
     promptUser()
-    .then((answers) => writeFileAsync('README.md', writeToFile(answers)))
-    .then(() => console.log('Successfully wrote to README.md'))
-    .catch((err) => console.error(err));
+    .then((data) => writeFileAsync('README.md', generateMarkdown(data)))
+    .then(() => console.log('Successfully created!'))
+    .catch((err) => console.error(err)); 
 };
+
+// const init = () => {
+//     promptUser()
+//         .then((data) => writeFileAsync('README.md', writeToFile(data)))
+//         .then(() => console.log('Successfully wrote to README.md'))
+//         .catch((err) => console.error(err));
+// };
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,9 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+}
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,9 +15,40 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.project_title}
 
-`;
+  # Good ReadMe Generator
+
+  #### Table of Contents
+  * [Project Description](#project-description)
+  * [Installation Instructions](#installation-instructions)
+  * [Usage Guidelines](#usage-guidelines)
+  * [License](#license)
+  * [Contribution Guidelines](#contribution-guidelines)
+  * [Test Instructions](#test-instructions)
+  * [Questions](#questions)
+
+  # Project Description 
+  * ${data.description}
+
+  # Installation Instructions
+
+  # Usage Guidelines
+  * ${data.usage}
+
+  # License
+  * This project is licensed under the ${data.license}
+
+  # Contribution Guidelines
+  * ${data.contributions}
+
+  # Test Instructions
+  * ${data.test_command}
+
+  # Questions
+  If you have any questions about the repom contact me directly at ${data.email}. 
+  You can find more of my work at [${data.github_username}](http://github.com/${data.github_username}).`;
 }
 
 module.exports = generateMarkdown;
